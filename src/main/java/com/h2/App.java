c74c2a2aad044334f4784d8c3dc71cb11271786c
yuri443 <yuririchacampos1995@gmail.com>
2021-04-29 09:18:31 -0300

    teste2

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,17 +1,21 @@
 package com.h2;
 
 /**
- * Hello world!!!!
+ * lembre-se que você começou esse programa para ajudar os alunos a entender lógica de programação de computadores!!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
 
-    public static int doubleTheNumber(int number) {
-        return -1;
+public class App
+{
+    public static void main(String[] args){System.out.println("Hallo World");}
+
+    public static int doubleTheNumber(int number){return 2 * number;}
+
+    private static int add(int[] numbers){
+        var sum = 0;
+        for(int number : numbers){
+            sum += number;
+        }
+        return sum;
     }
 }
